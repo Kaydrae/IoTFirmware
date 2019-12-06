8ac4c32a8cc858f483216abadc03628c10b27c28
Kaydrae <ondrae100@gmail.com>
2019-12-05 22:25:29 -0500

    AC Light Dimmer Code

---
+++ ACLightDimmer/ACLightDimmer.ino
@@ -0,0 +1,152 @@
+#include "hw_timer.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+#ifndef STASSID
+#define STASSID "RobotHouse"
+#define STAPSK  "R0botHous3Rule$"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+          
+const byte zcPin = 12;
+const byte pwmPin = 13;  
+
+byte fade = 1;
+byte state = 1;
+byte tarBrightness = 255;
+byte curBrightness = 0;
+byte zcState = 0; // 0 = ready; 1 = processing;
+
+void ICACHE_RAM_ATTR zcDetectISR();
+
+void setup() {
+  Serial.begin(115200);   
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  pinMode(zcPin, INPUT_PULLUP);
+  pinMode(pwmPin, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(zcPin), zcDetectISR, RISING);    // Attach an Interupt to Pin 2 (interupt 0) for Zero Cross Detection
+  hw_timer_init(NMI_SOURCE, 0);
+  hw_timer_set_func(dimTimerISR);
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+   ArduinoOTA.setHostname("Lab Main Lights");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ArduinoOTA.handle();
+  
+    if (Serial.available()){
+        int val = Serial.parseInt();
+        if (val>0){
+          tarBrightness =val;
+          Serial.println(tarBrightness);
+        }
+        
+    }
+}
+
+
+void dimTimerISR() {
+    if (fade == 1) {
+      if (curBrightness > tarBrightness || (state == 0 && curBrightness > 0)) {
+        --curBrightness;
+      }
+      else if (curBrightness < tarBrightness && state == 1 && curBrightness < 255) {
+        ++curBrightness;
+      }
+    }
+    else {
+      if (state == 1) {
+        curBrightness = tarBrightness;
+      }
+      else {
+        curBrightness = 0;
+      }
+    }
+    
+    if (curBrightness == 0) {
+      state = 0;
+      digitalWrite(pwmPin, 0);
+    }
+    else if (curBrightness == 255) {
+      state = 1;
+      digitalWrite(pwmPin, 1);
+    }
+    else {
+      digitalWrite(pwmPin, 1);
+    }
+    
+    zcState = 0;
+}
+
+void zcDetectISR() {
+  if (zcState == 0) {
+    zcState = 1;
+  
+    if (curBrightness < 255 && curBrightness > 0) {
+      digitalWrite(pwmPin, 0);
+      
+      int dimDelay = 30 * (255 - curBrightness) + 400;//400
+      hw_timer_arm(dimDelay);
+    }
+  }
+}
